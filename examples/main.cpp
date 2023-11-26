b7cffe9ebdf5dc023371300422173831158c9ea0
ndrewA <andrewbach1999@gmail.com>
2023-11-26 18:47:56 +0200

    Initial commit

---
+++ examples/main.cpp
@@ -0,0 +1,7 @@
+#include "header.h"
+
+int main()
+{
+    printHello();
+    return 0;
+}
---
+++ src/source.cpp
@@ -0,0 +1,8 @@
+#include "header.h"
+
+#include <iostream>
+
+void printHello()
+{
+    std::cout << "test123\n";
+}
